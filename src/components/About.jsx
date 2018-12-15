16112b5b317895832a6295757b2e6a169e6b22cb
unknown <mabbasidesign@yahoo.com>
2018-12-15 11:46:57 +0330

    build about page

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const About = () => {
+    return ( 
+        <div className="container">
+            <h4 className="center"> About </h4>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+                magnam. Maiores esse magni optio minus, rem, nisi eius 
+                voluptatibus adipisci quasi iste deserunt distinctio,
+                quam autem fugit molestias accusantium culpa! </p>
+        </div>
+     );
+}
+ 
+export default About;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 const Home = () => {
     return ( 
         <div className="container">
-            <h4 className="center"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur  </h4>
+            <h4 className="center"> Home </h4>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Ab cupiditate quaerat voluptas, dolore nisi modi architecto at,
+                voluptate explicabo soluta laboriosam reiciendis commodi nesciunt
+                accusamus velit similique odit fugit magni! </p>
         </div>
      );
 }
