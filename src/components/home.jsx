8cfa392d4294da3f68e86f1c64dcb1a13b251d1b
unknown <mabbasidesign@yahoo.com>
2018-12-15 11:42:11 +0330

    initiall

---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Home = () => {
+    return ( 
+        <div className="container">
+            <h4 className="center"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur  </h4>
+        </div>
+     );
+}
+ 
+export default Home;
