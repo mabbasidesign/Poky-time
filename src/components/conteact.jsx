c6dcf5652a9b312f14f0d4999842c47e80d598dd
unknown <mabbasidesign@yahoo.com>
2018-12-15 13:26:24 +0330

    create navbar component

---
+++ src/components/conteact.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const Contact = () => {
+    return ( 
+        <div className="container">
+            <h4 className="center"> Contact </h4>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+                magnam. Maiores esse magni optio minus, rem, nisi eius 
+                voluptatibus adipisci quasi iste deserunt distinctio,
+                quam autem fugit molestias accusantium culpa! </p>
+        </div>
+     );
+}
+ 
+export default Contact;
