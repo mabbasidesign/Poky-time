c6539931b45792377ebec7154048bdd1ab291f77
unknown <mabbasidesign@yahoo.com>
2018-12-16 13:58:41 +0330

    showing data using axios

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,18 +3,49 @@ import axios from 'axios';
 
 class Home extends Component {
 
+state = {
+    posts: []
+}
+
 componentDidMount(){
-    
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            console.log(res);
+            this.setState({
+                posts: res.data.slice(0, 10)
+            })
+        });
 }
 
    render(){
-    return ( 
+
+    const { posts } = this.state;
+    
+    const postList = posts.length ?
+    (
+        posts.map( post => {
+            return(
+                <div className="post card" key={post.id}>
+                    <div className="card-content">
+                        <span className="card-title">{post.title}</span>
+                        <p>{post.body}</p>
+                    </div>
+                </div>
+            )
+        })
+    ):
+    (
+        <div className="center">
+            No post yet
+        </div>
+    );
+
+
+
+    return (
         <div className="container">
             <h4 className="center"> Home </h4>
-            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Ab cupiditate quaerat voluptas, dolore nisi modi architecto at,
-                voluptate explicabo soluta laboriosam reiciendis commodi nesciunt
-                accusamus velit similique odit fugit magni! </p>
+            {postList}
         </div>
      );
    }
