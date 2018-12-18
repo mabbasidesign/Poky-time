b020bef880d280891403491be5198d32f73dcc80
unknown <mabbasidesign@yahoo.com>
2018-12-18 10:48:35 +0330

    complete router

--- src/components/hoc/rainbow.jsx
+++ src/components/hoc/rainbow.jsx
@@ -14,5 +14,5 @@ const Rainbow = (WrappedComponent) => {
     }
 
 }
- 
+
 export default Rainbow;
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,24 +1,74 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Post extends Component {   
 
     state = {
-        id: null
+        post: null
     }
 
     componentDidMount () {
-        console.log(this.props);
+        // console.log(this.props);
         let id = this.props.match.params.post_id;
-        this.setState({ id });
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+        .then(res => {
+            this.setState({
+                post: res.data
+            })
+            console.log(res);
+        });
     }
 
-    render() { 
+    render() {
+
+    const post = this.state.post ?
+    (
+        <div className="post">
+            <h4 className="center">{this.state.post.title}</h4>
+            <p>{this.state.post.body}</p>
+        </div>
+    ):
+    (
+        <div className="center"> Loading post ... </div>
+    );
+
         return ( 
             <div className="container">
-                <h4>{this.state.id}</h4>
+                { post }
             </div>
          );
     }
 }
 
 export default Post;
+
+
+
+
+
+
+
+
+
+// class Post extends Component {   
+
+//     state = {
+//         id: null
+//     }
+
+//     componentDidMount () {
+//         console.log(this.props);
+//         let id = this.props.match.params.post_id;
+//         this.setState({ id });
+//     }
+
+//     render() { 
+//         return ( 
+//             <div className="container">
+//                 <h4>{this.state.id}</h4>
+//             </div>
+//          );
+//     }
+// }
+
+// export default Post;
