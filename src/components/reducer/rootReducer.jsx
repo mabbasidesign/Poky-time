a89a5e643e50876752c15ac416ae2f635667c859
unknown <mabbasidesign@yahoo.com>
2018-12-21 17:06:17 +0330

    create roote reducer

---
+++ src/components/reducer/rootReducer.jsx
@@ -0,0 +1,15 @@
+// iniitState
+initeState = {
+    posts: [
+        {id: '1', title:'number 1', body:'recusandae consequuntur expedita et cum reprehenderit molestiae'},
+        {id: '2', title:'number 2', body:'recusandae consequuntur expedita et cum reprehenderit molestiae'},
+        {id: '3', title:'number 3', body:'recusandae consequuntur expedita et cum reprehenderit molestiae'}
+    ]
+}
+
+//rootReducer
+const rooteReducer = (state = initeState, action) => {
+    return state
+}
+
+export default rooteReducer;
