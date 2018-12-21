5417aa1367061dcf25981d849146cd1ecc5bfe3d
unknown <mabbasidesign@yahoo.com>
2018-12-21 20:26:53 +0330

    add map to props in post component

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Post extends Component {   
 
-    state = {
-        post: null
-    }
+    // state = {
+    //     post: null
+    // }
 
-    componentDidMount () {
-        // console.log(this.props);
-        let id = this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-        .then(res => {
-            this.setState({
-                post: res.data
-            })
-            console.log(res);
-        });
-    }
+    // componentDidMount () {
+    //     // console.log(this.props);
+    //     let id = this.props.match.params.post_id;
+    //     axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+    //     .then(res => {
+    //         this.setState({
+    //             post: res.data
+    //         })
+    //         console.log(res);
+    //     });
+    // }
 
     render() {
-
-    const post = this.state.post ?
+    console.log(this.props)
+    const post = this.props.post ?
     (
         <div className="post">
-            <h4 className="center">{this.state.post.title}</h4>
-            <p>{this.state.post.body}</p>
+            <h4 className="center">{this.props.post.title}</h4>
+            <p>{this.props.post.body}</p>
         </div>
     ):
     (
@@ -40,35 +41,22 @@ class Post extends Component {
     }
 }
 
-export default Post;
-
-
-
-
-
-
-
-
-
-// class Post extends Component {   
-
-//     state = {
-//         id: null
-//     }
-
-//     componentDidMount () {
-//         console.log(this.props);
-//         let id = this.props.match.params.post_id;
-//         this.setState({ id });
-//     }
-
-//     render() { 
-//         return ( 
-//             <div className="container">
-//                 <h4>{this.state.id}</h4>
-//             </div>
-//          );
+// const mapStateToProps = (state, ownProps) => {
+//     const id = ownProps.match.params.post_id;
+//     return {
+//         post: state.posts.find( (p) => {
+//             return p.id === id;
+//         })
 //     }
 // }
 
-// export default Post;
+const mapStateToProps = (state, ownProps) => {
+    let id = ownProps.match.params.post_id;
+    return {
+        post: state.posts.find((p) => {
+            return p.id === id
+        })
+    }
+}
+
+export default connect(mapStateToProps)(Post);
